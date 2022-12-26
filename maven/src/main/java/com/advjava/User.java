c6a65877e9a9e5e26ff7fadd57fc7f9a7e490035
newbieprogrammer17 <omshinde1706@gmail.com>
2022-12-27 01:04:07 +0530

    Data Access Object interface and user implementation created.

---
+++ maven/src/main/java/com/advjava/User.java
@@ -0,0 +1,32 @@
+package com.advjava;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Optional;
+
+public class User implements Dao{
+
+	@Override
+	public void save(Object t) {
+		
+		
+	}
+
+	@Override
+	public Optional findById(int id) {
+		
+		return null;
+	}
+
+	@Override
+	public void update(Object t) {
+			
+	}
+
+	@Override
+	public void delete(Object t) {
+		
+		
+	}
+
+}
