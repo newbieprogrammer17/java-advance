42cad6b1ff3faaa2aa527ff0b2d0b7644b5463d8
newbieprogrammer17 <omshinde1706@gmail.com>
2022-12-27 00:41:53 +0530

    database connection add

--- maven/src/main/java/com/advjava/App.java
+++ maven/src/main/java/com/advjava/App.java
@@ -1,5 +1,7 @@
 package com.advjava;
 
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,21 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        Database db = Database.instance();
+        
+        try {
+			db.connectDb();
+		} catch (SQLException e) {
+			System.out.println("Cannot Connect to Database.");
+		}
+        
+        System.out.println("Connected to Database.");
+        
+        try {
+			db.closeDb();
+		} catch (SQLException e) {
+			System.out.println("Cannot close Database Connection.");
+		}
     }
 }
